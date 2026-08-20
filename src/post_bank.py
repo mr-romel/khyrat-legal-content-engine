69d28d96a9d7d815df44828c5c15c9865c59884b
mr-romel <mahmoud.khyrat89@gmail.com>
2026-08-20 14:19:37 +0300

    Fix package import in Post Bank module

--- src/post_bank.py
+++ src/post_bank.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from datetime import datetime
 from typing import Any
 
-from sheets import get_values
+from src.sheets import get_values
 
 
 BANK_SHEET = "PostBank"
